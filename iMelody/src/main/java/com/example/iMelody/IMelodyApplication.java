81cb829f2a31a7890af9d1735bb7688815e4bf50
SethOberg <seth.a.oberg@gmail.com>
2023-02-17 14:29:05 +0100

    Initial commit

---
+++ iMelody/src/main/java/com/example/iMelody/IMelodyApplication.java
@@ -0,0 +1,13 @@
+package com.example.iMelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMelodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMelodyApplication.class, args);
+	}
+
+}
---
+++ iMelody/src/test/java/com/example/iMelody/IMelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iMelody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMelodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
