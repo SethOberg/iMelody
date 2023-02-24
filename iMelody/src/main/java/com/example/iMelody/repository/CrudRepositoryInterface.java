8f8fa9e279b9b83ec0a73e3127ecd9f884c5d9f0
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-02-24 14:37:32 +0100

    added documentation to the repositories

--- iMelody/src/main/java/com/example/iMelody/repository/CrudRepositoryInterface.java
+++ iMelody/src/main/java/com/example/iMelody/repository/CrudRepositoryInterface.java
@@ -3,13 +3,35 @@ package com.example.iMelody.repository;
 import java.util.List;
 
 public interface CrudRepositoryInterface<T, K> {
+    /**
+     * Retrieves all the entries in the database.
+     * @params none
+     * @return List<T>
+     */
     List<T> getAll();
+
+    /**
+     * Retrieves an entry based on the given id
+     * @param id
+     * @return T
+     */
     T getById(int id);
 
-    T getByName(String firstName, String lastName);
+    /**
+     * Adds a new entry to the database.
+     * @param object
+     * @return Int
+     */
     int insert(T object);
 
+    /**
+     * Updates an existing entry.
+     * @param object
+     * @return Int
+     */
     int update(T object);
+
+
 
     int delete(T object);
 
